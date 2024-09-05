872b3b660f03009dfc5495b5c4a09cb0c900b068
CowEstaris <vk_vm@ukr.net>
2024-09-05 13:29:56 +0300

    change project structure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# alsparse
+# ALSParse
+
+Simple parser for ALS (Ableton Live Set) files.
+Supports:
+- project metadata;
+- audio/midi/return/group/main tracks;
+- audio clips (start, end, name, color);
+- midi clips (start, end, name, color, notes);
+- automation (parameter name as a barely-human-readable string, value, time);
+
+Additionally there's some tools for working with ALS files:
+- `tools/dump.py` - dumps all the data from ALS file;
+- `tools/visualize/viz.py` - generates a video visualization of ALS file (as a rolling piano roll);
+
+Visit these tools for more information.
--- tools/visualize/viz.py
+++ tools/visualize/viz.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# @file util
-# @date 05-09-2024
+# @file dump.py
+# @date 03-09-2024
 # @author Maxim Kurylko <vk_vm@ukr.net>
 #
 
+import sys
+sys.path.append(".")
+
+from alsparse.alsparse import parse_file, MidiTrack, MasterTrack, ReturnTrack, \
+    GroupTrack, AudioTrack
+import os
+import argparse
 import logging
 import sys
+
+__description__ = "Simple CLI tool to dump information from Ableton project files"
+__version__ = "0.1.0"
+
 
 def setup_logging(args):
     class Fore:
@@ -39,3 +50,36 @@ def setup_logging(args):
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(logging.Formatter(get_format_string(not args.colorless, args.log == "debug")))
     logging.getLogger().addHandler(handler)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, prog=os.path.basename(__file__))
+    parser.description = __description__
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("-l", "--log", type=str, default="info", choices=["debug", "info", "warning", "error", "critical"],
+                        help="Log level. Note: 'debug' log level may print sensitive information,\n"
+                             "produce a lot of output and program may run slower/incorectly")
+    parser.add_argument("-c", "--colorless", action="store_true", help="Disable colored output")
+    parser.add_argument("input", help="Input file")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    setup_logging(args)
+
+    input_file = os.path.abspath(os.path.expanduser(args.input))
+    logging.info("Input file '%s'", input_file)
+
+    project = parse_file(input_file)
+    if not project:
+        logging.error("Failed to parse file '%s'", input_file)
+        return 1
+
+    logging.info("Parsed file \"%s\"", input_file)
+    logging.info("Project: \"%s\" (Daw: %s, %s)", project.get_name(), project.get_daw(), project.get_daw_version())
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
